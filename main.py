8ca867ad4f2a21c5a5120aba43527bf97faad694
billLee3 <bill.lee.third@gmail.com>
2025-09-01 13:38:17 -0500

    Create player class and some attributes

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
     game_over = False
     while game_over != True:
         for event in pygame.event.get():
@@ -18,6 +20,8 @@ def main():
         print(f"Screen height: {SCREEN_HEIGHT}")
         #Refreshes the screen
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
         
 
 if __name__ == "__main__":
