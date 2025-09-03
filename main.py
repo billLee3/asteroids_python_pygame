05101a090738d2e4c5d50393163ad3473fe9ebcb
billLee3 <bill.lee.third@gmail.com>
2025-09-02 22:31:13 -0500

    Player drawn on screen

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
     dt = 0.0
     game_over = False
     while game_over != True:
@@ -15,9 +17,7 @@ def main():
                 return
         #Fills the background
         pygame.Surface.fill(screen, (0, 0, 0))
-        print("Starting Asteroids!")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
+        player.draw(screen)
         #Refreshes the screen
         pygame.display.flip()
         dt = clock.tick(60)/1000
--- player.py
+++ player.py
@@ -4,9 +4,9 @@ from constants import PLAYER_RADIUS
 
 class Player(CircleShape):
     def __init__(self, x, y, PLAYER_RADIUS):
-        super().__init(PLAYER_RADIUS)
-        self.x = x
-        self.y = y
+        super().__init__(x, y, PLAYER_RADIUS)
+        # self.x = x
+        # self.y = y
         self.rotation = 0
 
     # in the player class
@@ -17,3 +17,6 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
