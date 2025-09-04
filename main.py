74e34410cdb6c33d9481719f682436102daf8d9e
billLee3 <bill.lee.third@gmail.com>
2025-09-03 21:41:35 -0500

    Reoriented group info for player

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+    
     dt = 0.0
     
-    updatable = pygame.sprite.Group(player)
-    drawable = pygame.sprite.Group(player)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
     AsteroidField.containers = (updatable)
 
     asteroidField = AsteroidField()
